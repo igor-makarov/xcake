af1e1fbe8f61dfc90d8106e8d611b49f051a7bf3
James Campbell <james@supmenow.com>
2015-10-10 23:49:32 +0100

    Setting project name.

--- lib/xcake/cakefile.rb
+++ lib/xcake/cakefile.rb
@@ -1,8 +1,11 @@
 module Xcake
   class Cakefile
+
+    attr_accessor :project_name
     attr_accessor :targets
 
-    def initialize(&block)
+    def initialize(name="Project", &block)
+      self.project_name = name
       self.targets = []
 
       block.call(self) if block_given?
--- spec/cakefile_spec.rb
+++ spec/cakefile_spec.rb
@@ -3,6 +3,18 @@ require 'spec_helper'
 module Xcake
   describe Cakefile do
 
+    it "should have a default project name" do
+      cakefile = Cakefile.new
+
+      expect(cakefile.project_name).to eq("Project")
+    end
+
+    it "should set project name" do
+      cakefile = Cakefile.new("Test")
+
+      expect(cakefile.project_name).to eq("Test")
+    end
+
     it "should initialize targets" do
       cakefile = Cakefile.new
 
