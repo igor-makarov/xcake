45f6900eb04c2419f4ab2e5a65f8f33ad5302313
James Campbell <james@supmenow.com>
2015-10-10 23:21:32 +0100

    Update help info.

--- lib/xcake/command.rb
+++ lib/xcake/command.rb
@@ -2,7 +2,8 @@ require 'claide'
 
 module Xcake
   class Command < CLAide::Command
-
+    
+    self.command = 'xcake'
     self.description = 'Create and maintain Xcode project files easily.'
 
     def run
