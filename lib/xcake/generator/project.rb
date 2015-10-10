431fa1f974350f8e79267e42932ce420a05c5185
James Campbell <james@supmenow.com>
2015-10-11 00:05:18 +0100

    Removes spacing.

--- lib/xcake/generator/project.rb
+++ lib/xcake/generator/project.rb
@@ -7,12 +7,10 @@ module Xcake
       attr_accessor :cakefile
 
       def initialize(cakefile)
-
         self.cakefile = cakefile
       end
 
       def build
-
         project = Xcodeproj::Project.new "./#{self.cakefile.project_name}.xcodeproj"
         project.save
         puts "Build :)"
