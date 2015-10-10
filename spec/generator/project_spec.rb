0734dbd7252c0497cac04b0e1929ec91d5254b20
James Campbell <james@supmenow.com>
2015-10-11 00:27:08 +0100

    Specs fixed.

--- spec/generator/project_spec.rb
+++ spec/generator/project_spec.rb
@@ -9,6 +9,13 @@ module Xcake
 
         expect(project.cakefile).to eq(cakefile)
       end
+
+      it 'stores the cakefile' do
+        cakefile = Cakefile.new
+        project = Project.new(cakefile)
+
+        expect(project.output_filepath).to eq("./Project.xcodeproj")
+      end
     end
   end
 end
