7e2f097f1121ecf555e953ad8f60d863c98f6ba7
James Campbell <james@supmenow.com>
2015-10-11 08:38:09 +0100

    Build configuration.

--- lib/xcake/generator/build_configuration.rb
+++ lib/xcake/generator/build_configuration.rb
@@ -3,6 +3,10 @@ require 'xcodeproj'
 module Xcake
   module Generator
     class BuildConfiguration
+      attr_accessor :build_configuration
+
+      def initialize(build_configuration)
+      end
 
     end
   end
