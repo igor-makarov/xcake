04db6994e37d24f6b4b65160c8540f16ea696f54
James Campbell <james@supmenow.com>
2015-10-11 07:56:52 +0100

    Removes old code.

--- lib/xcake/generator/project.rb
+++ lib/xcake/generator/project.rb
@@ -17,23 +17,6 @@ module Xcake
       def build
         project = Xcode::Project.new(output_filepath, true)
         project.setup_for_xcake
-
-        # root_object.remove_referrer(self) if root_object
-        # @root_object = new(PBXProject)
-        # root_object.add_referrer(self)
-        #
-        # root_object.main_group = new(PBXGroup)
-        # root_object.product_ref_group = root_object.main_group.new_group('Products')
-        #
-        # config_list = new(XCConfigurationList)
-        # root_object.build_configuration_list = config_list
-        # config_list.default_configuration_name = 'Release'
-        # config_list.default_configuration_is_visible = '0'
-        # add_build_configuration('Debug', :debug)
-        # add_build_configuration('Release', :release)
-        #
-        # new_group('Frameworks')
-
         project.save
       end
     end
