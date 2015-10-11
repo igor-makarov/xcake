4d0c3fdda7ee240b8dfd25647d32691f56eadceb
James Campbell <james@supmenow.com>
2015-10-11 08:49:21 +0100

    Moves configuration.

--- lib/xcake/generator/project.rb
+++ lib/xcake/generator/project.rb
@@ -17,6 +17,22 @@ module Xcake
       def build
         project = Xcode::Project.new(output_filepath, true)
         project.setup_for_xcake
+
+        #TODO: Create subclass that manages all of this
+                #   build_configuration = new(XCBuildConfiguration)
+                # build_configuration.name = name
+                # common_settings = Constants::PROJECT_DEFAULT_BUILD_SETTINGS
+                # settings = ProjectHelper.deep_dup(common_settings[:all])
+                # settings.merge!(ProjectHelper.deep_dup(common_settings[type]))
+                # build_configuration.build_settings = settings
+                # build_configuration_list.build_configurations << build_configuration
+                # build_configuration
+
+                  # config_list.default_configuration_name = 'Release'
+                  # config_list.default_configuration_is_visible = '0'
+                  # add_build_configuration('Debug', :debug)
+                  # add_build_configuration('Release', :release)
+
         project.save
       end
     end
--- lib/xcake/xcode/project.rb
+++ lib/xcake/xcode/project.rb
@@ -8,14 +8,8 @@ module Xcake
           root_object = new(Project::Object::PBXProject)
           root_object.add_referrer(self)
 
-#TODO: Create subclass that manages all of this
           config_list = new(XCConfigurationList)
           root_object.build_configuration_list = config_list
-
-          # config_list.default_configuration_name = 'Release'
-          # config_list.default_configuration_is_visible = '0'
-          # add_build_configuration('Debug', :debug)
-          # add_build_configuration('Release', :release)
 
           @root_object = root_object
         end
