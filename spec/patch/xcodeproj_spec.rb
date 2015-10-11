7883e3b707e500f2cea70126cc646a06d2bec540
James Campbell <james@supmenow.com>
2015-10-11 07:01:03 +0100

    All tests passing.

--- spec/patch/xcodeproj_spec.rb
+++ spec/patch/xcodeproj_spec.rb
@@ -4,7 +4,7 @@ module Xcodeproj
   describe Project do
 
     it "should set the root object when setup" do
-      project = Xcodeproj::Project.new(output_filepath, true)
+      project = Xcodeproj::Project.new(".", true)
       project.setup_for_xcake
 
       expect(project.root_object).to_not be(nil)
