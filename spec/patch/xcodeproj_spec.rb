ab9008b25f6b87c04cde8891a3a91128a5c468f2
James Campbell <james@supmenow.com>
2015-10-11 07:38:30 +0100

    Preparing tests.

--- spec/patch/xcodeproj_spec.rb
+++ spec/patch/xcodeproj_spec.rb
@@ -13,7 +13,13 @@ module Xcodeproj
     it "should return archive version as last known" do
       project = Xcodeproj::Project.new(".", true)
 
-      expect(project.archive_version).to eq(Constants::LAST_KNOWN_ARCHIVE_VERSION.to_s)
+      expect(project.archive_version).to eq(Constants::LAST_KNOWN_ARCHIVE_VERSION)
+    end
+
+    it "should return pbject version as default" do
+      project = Xcodeproj::Project.new(".", true)
+
+      expect(project.object_version).to eq(Constants::DEFAULT_OBJECT_VERSION)
     end
   end
 end
