7029a615d87efe9b615cafbc234e687d6febfe8c
James Campbell <james@supmenow.com>
2015-10-16 17:49:30 +0100

    Latest comments update.

--- lib/xcake/generator/file.rb
+++ lib/xcake/generator/file.rb
@@ -37,9 +37,10 @@ module Xcake
 
         root_node.traverse do |n|
           if ::File.directory?("./#{n.path}")
+#TODO: Handle different types and tweaking so group defaults to select targets in xcode
             main_group.find_subpath(n.path, true)
           else
-#TODO: Handle different types and adding to phases
+#TODO: Handle different types (i.e Frameworks and xcassets) and adding to phases for targets
             group = main_group.find_subpath(n.parent.path, true)
             group.new_reference(n.path)
           end
