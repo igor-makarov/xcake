c2636da03afab6e450c301e5d9dc0872ff97406c
James Campbell <james@supmenow.com>
2015-10-17 21:29:13 +0100

    Uses modern syntax.

--- spec/generator/file/node_spec.rb
+++ spec/generator/file/node_spec.rb
@@ -4,7 +4,15 @@ module Xcake
   module Generator
     class File
       describe Node do
+        it 'initilizes the children' do
+          node = Node.new
+          expect(node.children).not_to be_nil
+        end
 
+        it 'initilizes the targets' do
+          node = Node.new
+          expect(node.targets).not_to be_nil
+        end
       end
     end
   end
--- spec/generator/project_spec.rb
+++ spec/generator/project_spec.rb
@@ -4,18 +4,17 @@ module Xcake
   module Generator
     describe Project do
       it 'stores the cakefile' do
-        cakefile = class_double('Cakefile')
+        cakefile = double('Cakefile')
         generator = Project.new(cakefile)
 
         expect(generator.cakefile).to eq(cakefile)
       end
 
       it 'generates the correct output filepath' do
-        cakefile = class_double('Cakefile')
-        cakefile.stub(:project_name).and_return('Project')
+        cakefile = double('Cakefile')
+        allow(cakefile).to receive(:project_name) { 'Project' }
 
         generator = Project.new(cakefile)
-
         expect(generator.output_filepath).to eq("./Project.xcodeproj")
       end
 
