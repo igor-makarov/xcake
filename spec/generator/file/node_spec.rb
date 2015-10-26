8b5ade4303482041672bd32674f51b5ae915276a
James Campbell <james@supmenow.com>
2015-10-26 13:14:23 +0000

    Test wording tweaks.

--- spec/generator/file/node_spec.rb
+++ spec/generator/file/node_spec.rb
@@ -198,7 +198,7 @@ module Xcake
                 @node.remove_children_with_path("./folder/file", @target)
               end
 
-              it 'should keep one child' do
+              it 'should keep folder' do
                 expect(@node.children.count).to eq(1)
               end
 
