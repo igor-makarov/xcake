223ab3cd36cc27264f9c98e047885cce1028a0a2
James Campbell <james@supmenow.com>
2015-10-27 21:17:07 +0000

    Extends CoreFoundation.

--- lib/xcake/xcode/plist_helper.rb
+++ lib/xcake/xcode/plist_helper.rb
@@ -1,49 +1,44 @@
 require 'fiddle'
 require 'xcodeproj'
 
-module Xcake
-  module Xcode
-    module CoreFoundation
+CoreFoundation.module_eval do
 
-      extend CoreFoundation
+    NULL = Fiddle::NULL
 
-      VoidPointer = Fiddle::TYPE_VOIDP
-      UInt32 = -Fiddle::TYPE_INT
+    VoidPointer = Fiddle::TYPE_VOIDP
+    UInt32 = -Fiddle::TYPE_INT
 
-      CFTypeRef = VoidPointer
-      CFTypeRefPointer = VoidPointer
-      CFIndex = Fiddle::TYPE_LONG
-      CFOptionFlags = UInt32
-      CFPropertyListFormatPointer = VoidPointer
-      UInt8Pointer = VoidPointer
+    CFTypeRef = VoidPointer
+    CFTypeRefPointer = VoidPointer
+    CFIndex = Fiddle::TYPE_LONG
+    CFOptionFlags = UInt32
+    CFPropertyListFormatPointer = VoidPointer
+    UInt8Pointer = VoidPointer
 
-      extern :CFDataCreate, [CFTypeRef, UInt8Pointer, CFIndex], CFTypeRef
-      extern :CFPropertyListCreateWithStream, [CFTypeRef, CFTypeRef, CFIndex, CFOptionFlags, CFPropertyListFormatPointer, CFTypeRefPointer], CFTypeRef
+    extern :CFDataCreate, [CFTypeRef, UInt8Pointer, CFIndex], CFTypeRef
+    extern :CFPropertyListCreateWithStream, [CFTypeRef, CFTypeRef, CFIndex, CFOptionFlags, CFPropertyListFormatPointer, CFTypeRefPointer], CFTypeRef
 
-      def self.RubyStringPropertyListRead(string)
+    def self.RubyStringPropertyListRead(string)
 
-        string = RubyStringToCFString(string)
-        data = CFDataCreate(NULL,
-                            string.to_value,
-                            string.size)
+      string = RubyStringToCFString(string)
+      data = CFDataCreate(NULL,
+                          string.to_value,
+                          string.size)
 
-        error_ptr = CFTypeRefPointer()
-        plist = CFPropertyListCreateWithData(NULL,
-                                             data,
-                                             0,
-                                             KCFPropertyListImmutable,
-                                             NULL,
-                                             error_ptr)
-        CFReadStreamClose(stream)
-
-        if plist.null?
-          description = CFCopyDescription(error_ptr.ptr)
-          raise IOError, "Unable to read plist data: #{description}"
-        elsif CFGetTypeID(plist) != CFDictionaryGetTypeID()
-          raise TypeError, 'Expected a plist with a dictionary root object.'
-        end
-
-        CFDictionaryToRubyHash(plist)
+      error_ptr = CFTypeRefPointer()
+      plist = CFPropertyListCreateWithData(NULL,
+                                           data,
+                                           0,
+                                           KCFPropertyListImmutable,
+                                           NULL,
+                                           error_ptr)
+      if plist.null?
+        description = CFCopyDescription(error_ptr.ptr)
+        raise IOError, "Unable to read plist data: #{description}"
+      elsif CFGetTypeID(plist) != CFDictionaryGetTypeID()
+        raise TypeError, 'Expected a plist with a dictionary root object.'
       end
-  end
+
+      CFDictionaryToRubyHash(plist)
+    end
 end
