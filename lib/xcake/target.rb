29af1cd4e785c8ebb1bb4bb2f7bc096d35efc71d
James Campbell <james@supmenow.com>
2015-10-29 19:41:05 +0000

    Preparing target code.

--- lib/xcake/target.rb
+++ lib/xcake/target.rb
@@ -7,26 +7,27 @@ module Xcake
     attr_accessor :deployment_target
     attr_accessor :include_files
     attr_accessor :exclude_files
+    attr_accessor :system_frameworks
 
     def initialize(&block)
       block.call(self) if block_given?
-    end
 
-    #TODO: Setup defaults
-    # TODO: Figure out how to pull this off
-    #
-    # target.build_configuration_list = configuration_list(project, platform, deployment_target, type, language)
-    #
-    # # Product
-    # product = product_group.new_product_ref_for_target(name, type)
-    # target.product_reference = product
-    #
-    # # Build phases
-    # target.build_phases << project.new(PBXSourcesBuildPhase)
-    # target.build_phases << project.new(PBXFrameworksBuildPhase)
-    #
-    # # Frameworks
-    # framework_name = (platform == :osx) ? 'Cocoa' : 'Foundation'
-    # target.add_system_framework(framework_name)
+      #TODO: Setup defaults
+      # TODO: Figure out how to pull this off
+      #
+      # target.build_configuration_list = configuration_list(project, platform, deployment_target, type, language)
+      #
+      # # Product
+      # product = product_group.new_product_ref_for_target(name, type)
+      # target.product_reference = product
+      #
+      # # Build phases
+      # target.build_phases << project.new(PBXSourcesBuildPhase)
+      # target.build_phases << project.new(PBXFrameworksBuildPhase)
+      #
+      # # Frameworks
+      # framework_name = (platform == :osx) ? 'Cocoa' : 'Foundation'
+      # target.add_system_framework(framework_name)
+    end
   end
 end
