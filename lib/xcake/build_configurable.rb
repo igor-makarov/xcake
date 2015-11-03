e13f880bdb2552e6f4e3ad03a77f7e6795b17ef2
James Campbell <james@supmenow.com>
2015-11-03 19:17:45 +0000

    Setting build configurations done.

--- lib/xcake/build_configurable.rb
+++ lib/xcake/build_configurable.rb
@@ -3,7 +3,7 @@ module Xcake
     attr_accessor :build_configurations
 
     def build_configurations
-      @build_configurations || []
+      @build_configurations ||= []
     end
 
     def build_configuration(name)
