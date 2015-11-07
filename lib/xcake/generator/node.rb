5b1bd4fded6a052dcc20de2098cc3b03fb32d495
James Campbell <james@supmenow.com>
2015-11-07 23:43:38 +0000

    Testing mdls.

--- lib/xcake/generator/node.rb
+++ lib/xcake/generator/node.rb
@@ -96,6 +96,7 @@ module Xcake
       end
 
       def type_tree
+        puts `mdls`
         ::CoreFoundation.RubyStringPropertyListRead(`mdls -plist - -name kMDItemContentTypeTree #{self.path}`)["kMDItemContentTypeTree"]
       end
 
