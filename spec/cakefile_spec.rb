eeef5c7d3e0ef3e128858524258505d02e5730a8
James Campbell <james@supmenow.com>
2015-11-10 13:38:43 +0000

    Fixes failing test.

--- spec/cakefile_spec.rb
+++ spec/cakefile_spec.rb
@@ -40,8 +40,33 @@ module Xcake
         expect(@cakefile.targets.count).to eq(1)
       end
 
-      it "should store set type to application" do
+      it "should set type to application" do
         expect(@target.type).to eq(:application)
+      end
+
+      it "should set platform" do
+        expect(@target.platform).to eq(:ios)
+      end
+
+      it "should set deployment target" do
+        expect(@target.deployment_target).to eq(8.0)
+      end
+
+      it "should default to objective c" do
+        expect(@target.language).to eq(:objc)
+      end
+
+      context "when specifying language" do
+
+        before :each do
+          @cakefile = Cakefile.new do |c|
+            @target = c.application_for :ios, 8.0, :swift
+          end
+        end
+        
+        it "should set lanuggae" do
+          expect(@target.language).to eq(:swift)
+        end
       end
     end
 
