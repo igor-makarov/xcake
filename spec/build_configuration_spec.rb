1708b3a5676b362843f477145a748aae74d145a3
James Campbell <james@supmenow.com>
2015-11-10 14:05:37 +0000

    Build configuration tested.

--- spec/build_configuration_spec.rb
+++ spec/build_configuration_spec.rb
@@ -3,10 +3,20 @@ require 'spec_helper'
 module Xcake
   describe BuildConfiguration do
 
-    it "should set the name when created" do
-      name = :configuration
-      configuration = BuildConfiguration.new(name)
-      expect(configuration.name).to eq(name.to_s)
+    context "when created" do
+
+      before :each do
+        @name = :configuration
+        @configuration = BuildConfiguration.new(@name)
+      end
+
+      it "should set the name" do
+        expect(@configuration.name).to eq(@name.to_s)
+      end
+
+      it "should set the settings" do
+        expect(@configuration.settings).not_to be(nil)
+      end
     end
   end
 end
