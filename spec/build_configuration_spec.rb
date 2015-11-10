ef6b476c333fbd3c2bdcab6dd204d7f247996f09
James Campbell <james@supmenow.com>
2015-11-10 14:03:27 +0000

    Change to test symbol.

--- spec/build_configuration_spec.rb
+++ spec/build_configuration_spec.rb
@@ -4,9 +4,9 @@ module Xcake
   describe BuildConfiguration do
 
     it "should set the name when created" do
-      name = "Configuration"
+      name = :configuration
       configuration = BuildConfiguration.new(name)
-      expect(configuration.name).to eq(name)
+      expect(configuration.name).to eq(name.to_s)
     end
   end
 end
