96556079c2bcb9aa33cc4d7bb23e1d79c17cc174
James Campbell <james@supmenow.com>
2015-11-10 13:58:11 +0000

    Adds in configurations.

--- spec/build_configurable_spec.rb
+++ spec/build_configurable_spec.rb
@@ -2,16 +2,38 @@ require 'spec_helper'
 
 module Xcake
   describe BuildConfigurable do
-    context "when creating debug coniguration" do
 
+    before :each do
+      @build_configurable = Object.new
+      @build_configurable.extend(BuildConfigurable)
     end
 
-    context "when creating release coniguration" do
+    context "when creating debug configuration" do
 
+      before :each do
+        @build_configuration = @build_configurable.debug_build_configuration :debug
+      end
+
+      it "should store build configuration" do
+        expect(@build_configurable.debug_build_configurations.count).to eq(1)
+      end
     end
 
-    context "when accessing all coniguration" do
+    context "when creating release configuration" do
 
+      before :each do
+        @build_configuration = @build_configurable.release_build_configuration :release
+      end
+
+      it "should store build configuration" do
+        expect(@build_configurable.release_build_configurations.count).to eq(1)
+      end
     end
+
+    # context "when accessing all configuration" do
+    #   it "should store new one" do
+    #
+    #   end
+    # end
   end
 end
