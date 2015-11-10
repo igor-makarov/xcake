683cff7726b2b45045930d48b293468bb22af443
James Campbell <james@supmenow.com>
2015-11-10 14:00:29 +0000

    All tests passing.

--- spec/build_configurable_spec.rb
+++ spec/build_configurable_spec.rb
@@ -17,6 +17,17 @@ module Xcake
       it "should store build configuration" do
         expect(@build_configurable.debug_build_configurations.count).to eq(1)
       end
+
+      context "that already exists" do
+
+        before :each do
+          @duplicate_build_configuration = @build_configurable.debug_build_configuration :debug
+        end
+
+        it "should return same build configuration" do
+          expect(@duplicate_build_configuration).to be(@build_configuration)
+        end
+      end
     end
 
     context "when creating release configuration" do
@@ -27,6 +38,17 @@ module Xcake
 
       it "should store build configuration" do
         expect(@build_configurable.release_build_configurations.count).to eq(1)
+      end
+
+      context "that already exists" do
+
+        before :each do
+          @duplicate_build_configuration = @build_configurable.release_build_configuration :release
+        end
+
+        it "should return same build configuration" do
+          expect(@duplicate_build_configuration).to be(@build_configuration)
+        end
       end
     end
 
