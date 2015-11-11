58af733b61877b46da7a3b48367d7e047d0672b0
James Campbell <james@supmenow.com>
2015-11-11 10:45:10 +0000

    Preparing for processing of targets.

--- lib/xcake/project_structure_resolver.rb
+++ lib/xcake/project_structure_resolver.rb
@@ -2,5 +2,17 @@ module Xcake
   class ProjectStructureResolver
 
     include Visitor
+
+    def visit_cakefile(cakefile)
+    end
+
+    def leave_cakefile(cakefile)
+    end
+
+    def visit_target(target)
+    end
+
+    def leave_target(target)
+    end
   end
 end
