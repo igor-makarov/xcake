0abc9d19797a22d729db3c7bfd700ffe802da099
James Campbell <james@supmenow.com>
2015-11-12 23:53:53 +0000

    Fxes indentation.

--- lib/xcake/generator/build_phase/compile_source_build_phase.rb
+++ lib/xcake/generator/build_phase/compile_source_build_phase.rb
@@ -1,12 +1,14 @@
 module Xcake
-  class CompileSourceBuildPhase < BuildPhase
+  module Generator
+    class CompileSourceBuildPhase < BuildPhase
 
-    def self.can_install_path(file_reference)
-      [".c", ".m", ".mm", ".cpp", ".swift"].include?(File.extname(file_reference.path))
-    end
+      def self.can_install_path(file_reference)
+        [".c", ".m", ".mm", ".cpp", ".swift"].include?(File.extname(file_reference.path))
+      end
 
-    def install_for_target(target)
-      target.source_build_phase.add_file_reference(@file_reference)
+      def install_for_target(target)
+        target.source_build_phase.add_file_reference(@file_reference)
+      end
     end
   end
 end
--- lib/xcake/generator/build_phase/copy_resources_build_phase.rb
+++ lib/xcake/generator/build_phase/copy_resources_build_phase.rb
@@ -1,13 +1,15 @@
 module Xcake
-  class CopyResourcesBuildPhase < BuildPhase
+  module Generator
+    class CopyResourcesBuildPhase < BuildPhase
 
-    def self.can_install_file_reference(file_reference)
-      File.directory?(file_reference.path) == false &&
-      [".h", ".hpp"].include?(File.extname(file_reference.path)) == false
-    end
+      def self.can_install_file_reference(file_reference)
+        File.directory?(file_reference.path) == false &&
+        [".h", ".hpp"].include?(File.extname(file_reference.path)) == false
+      end
 
-    def visit_target(target)
-      target.resources_build_phase.add_file_reference(@file_reference)
+      def visit_target(target)
+        target.resources_build_phase.add_file_reference(@file_reference)
+      end
     end
   end
 end
