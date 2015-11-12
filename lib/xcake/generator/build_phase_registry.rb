bd68f0e61a0fb0299eeadda2668238a489ae98ee
James Campbell <james@supmenow.com>
2015-11-12 23:57:04 +0000

    All build phase code working.

--- lib/xcake/generator/build_phase_registry.rb
+++ lib/xcake/generator/build_phase_registry.rb
@@ -1,20 +1,22 @@
 module Xcake
-  class FileReferenceInstaller
-    module Registry
+  module Generator
+    class BuildPhase
+      module Registry
 
-      def self.build_phase_generators
-        [
-            CompileSourceBuildPhase,
-            CopyResourcesBuildPhase
-        ]
-      end
-
-      def self.generator_for_file_reference(file_reference)
-        generator_class = self.build_phase_generators.find do |g|
-          g.can_install_file_reference(file_reference)
+        def self.build_phase_generators
+          [
+              CompileSourceBuildPhase,
+              CopyResourcesBuildPhase
+          ]
         end
 
-        generator = generator_class.new(file_reference)
+        def self.generator_for_file_reference(file_reference)
+          generator_class = self.build_phase_generators.find do |g|
+            g.can_install_file_reference(file_reference)
+          end
+
+          generator = generator_class.new(file_reference)
+        end
       end
     end
   end
