674493162d99b6f3007aa38677463ee1d8b8d8f2
James Campbell <james@supmenow.com>
2015-11-13 18:36:33 +0000

    Almost all tests passing.

--- spec/generator/build_phase_registry_spec.rb
+++ spec/generator/build_phase_registry_spec.rb
@@ -4,7 +4,6 @@ module Xcake
   module Generator
     class BuildPhase
       describe Registry do
-
         it 'should return registered generators in correct order' do
           expect(Registry.build_phase_generators).to eq([
               CompileSourceBuildPhase,
