eb8b6cb0fedcf1efb9a2fc4c2f6342f21c1d0150
James Campbell <james@supmenow.com>
2015-11-13 09:04:56 +0000

    Removes old tests.

--- spec/generator/build_configuration_spec.rb
+++ spec/generator/build_configuration_spec.rb
@@ -3,12 +3,7 @@ require 'spec_helper'
 module Xcake
   module Generator
     describe BuildConfiguration do
-      it 'stores the project' do
-        project = double()
-        generator = BuildConfiguration.new(project, nil, nil)
 
-        expect(generator.project).to eq(project)
-      end
     end
   end
 end
--- spec/generator/target_spec.rb
+++ spec/generator/target_spec.rb
@@ -3,20 +3,6 @@ require 'spec_helper'
 module Xcake
   module Generator
     describe Target do
-      it 'stores the cakefile' do
-        cakefile = double()
-        generator = Target.new(cakefile, nil)
-
-        expect(generator.cakefile).to eq(cakefile)
-      end
-
-      it 'stores the project' do
-        project = double()
-        generator = Target.new(nil, project)
-
-        expect(generator.project).to eq(project)
-      end
-
       #Test Generating Targets
     end
   end
