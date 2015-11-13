977e04f535af14d38dd5549bc22a162e39419cd0
James Campbell <james@supmenow.com>
2015-11-13 08:51:29 +0000

    ATarget settings test.

--- spec/target_spec.rb
+++ spec/target_spec.rb
@@ -39,6 +39,10 @@ module Xcake
       target.language = :objc
 
       settings = Xcodeproj::Project::ProjectHelper.common_build_settings(:debug, target.platform, target.deployment_target.to_s, target.type, target.language)
+      settings.merge!({
+        "INFOPLIST_FILE" => "./$(PRODUCT_NAME)/Supporting Files/Info.plist"
+        })
+
       expect(target.default_debug_settings).to eq(settings)
     end
 
@@ -51,6 +55,9 @@ module Xcake
       target.language = :objc
 
       settings = Xcodeproj::Project::ProjectHelper.common_build_settings(:release, target.platform, target.deployment_target.to_s, target.type, target.language)
+      settings.merge!({
+        "INFOPLIST_FILE" => "./$(PRODUCT_NAME)/Supporting Files/Info.plist"
+        })
       expect(target.default_release_settings).to eq(settings)
     end
   end
