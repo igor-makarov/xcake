0380c16eb3ebb57bdd7a5125ced5bcd9d9064ffd
James Campbell <james@supmenow.com>
2015-11-19 10:12:06 +0000

    Adding initial build configuration tests.

--- spec/generator/build_configuration_spec.rb
+++ spec/generator/build_configuration_spec.rb
@@ -4,6 +4,31 @@ module Xcake
   module Generator
     describe BuildConfiguration do
 
+      it "should create a new XCBuildConfiguration" do
+
+        project = double()
+        expect(project).to receive(:new).with(Xcodeproj::Project::Object::XCBuildConfiguration)
+
+        build_configuration_target = double()
+        build_configuration = double()
+
+        generator = BuildConfiguration.new(project, build_configuration_target)
+        generator.visit_buildconfiguration(build_configuration)
+      end
+
+      #test sets name
+
+      #test sets build settings
+
+      #test add build configurations.
+
+
+
+      # build_configuration.name = configuration.name
+      # build_configuration.build_settings = configuration.settings
+      #
+      # @build_configuration_target.build_configurations << build_configuration
+
     end
   end
 end
