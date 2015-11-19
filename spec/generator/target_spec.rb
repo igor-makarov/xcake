2fabecace692cb94660e98a5defec23130755e51
James Campbell <james@supmenow.com>
2015-11-19 15:53:17 +0000

    Preparing include and excluding.

--- spec/generator/target_spec.rb
+++ spec/generator/target_spec.rb
@@ -5,8 +5,8 @@ module Xcake
     describe Target do
 
       before :each do
-        @project = double()
-        @root_node = double()
+        @project = double().as_null_object
+        @root_node = double().as_null_object
 
         @generator = Target.new(@project, @root_node)
       end
@@ -23,6 +23,35 @@ module Xcake
         it "should create a new target" do
           target = double().as_null_object
           expect(@project).to receive(:new_target).with(target)
+
+          @generator.visit_target(target)
+        end
+
+        it "should include files" do
+
+          native_target = double()
+          allow(@project).to receive(:new_target).and_return(native_target)
+
+          target = double().as_null_object
+          allow(target).to receive(:include_files).and_return('**/*.swift')
+          allow(Dir).to receive(:glob).with(any_args).and_return([])
+          allow(Dir).to receive(:glob).with('*/*.swift').and_return(['File.swift'])
+
+          expect(@root_node).to receive(:create_children_with_path).with('File.swift', native_target)
+
+          @generator.visit_target(target)
+        end
+
+        it "should exclude files" do
+          native_target = double()
+          allow(@project).to receive(:new_target).and_return(native_target)
+
+          target = double().as_null_object
+          allow(target).to receive(:exclude_files).and_return('**/*.swift')
+          allow(Dir).to receive(:glob).with(any_args).and_return([])
+          allow(Dir).to receive(:glob).with('*/*.swift').and_return(['File.swift'])
+
+          expect(@root_node).to receive(:remove_children_with_path).with('File.swift', native_target)
 
           @generator.visit_target(target)
         end
