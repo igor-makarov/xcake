941e547751e1a963c0cf89d41277284e0a2caa87
James Campbell <james@supmenow.com>
2015-11-19 10:27:08 +0000

    Initial project test works.

--- spec/generator/build_configuration_spec.rb
+++ spec/generator/build_configuration_spec.rb
@@ -6,11 +6,12 @@ module Xcake
 
       it "should create a new XCBuildConfiguration" do
 
+        xcode_build_configuration = double().as_null_object
         project = double()
-        expect(project).to receive(:new).with(Xcodeproj::Project::Object::XCBuildConfiguration)
+        expect(project).to receive(:new).with(Xcodeproj::Project::Object::XCBuildConfiguration).and_return(xcode_build_configuration)
 
-        build_configuration_target = double()
-        build_configuration = double()
+        build_configuration_target = double().as_null_object
+        build_configuration = double().as_null_object
 
         generator = BuildConfiguration.new(project, build_configuration_target)
         generator.visit_buildconfiguration(build_configuration)
