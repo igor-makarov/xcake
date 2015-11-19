f6ce918468abb6a404e7db55e5e6a37e532ba7b8
James Campbell <james@supmenow.com>
2015-11-19 09:59:04 +0000

    Copy resources fully tested.

--- spec/generator/build_phase/copy_resource_build_phase_spec.rb
+++ spec/generator/build_phase/copy_resource_build_phase_spec.rb
@@ -5,12 +5,16 @@ module Xcake
     describe CopyResourcesBuildPhase do
 
       #TODO: Get test working
-      # it "should not able to install folder" do
-      #   file_reference = double()
-      #   allow(file_reference).to receive(:path) { 'Folder/' }
-      #
-      #   expect(CopyResourcesBuildPhase.can_install_file_reference(file_reference)).to be(false)
-      # end
+      it "should not able to install folder" do
+
+        folder_path = 'Folder/'
+
+        file_reference = double()
+        allow(file_reference).to receive(:path) { folder_path }
+        allow(File).to receive(:directory?).with(folder_path).and_return(true)
+
+        expect(CopyResourcesBuildPhase.can_install_file_reference(file_reference)).to be(false)
+      end
 
       it "should be able to install non source code file" do
         file_reference = double()
