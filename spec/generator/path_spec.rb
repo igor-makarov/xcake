cd10c8d572a67388b6245a3446f8b0100a76ec3f
James Campbell <james@supmenow.com>
2015-11-19 11:40:41 +0000

    All Node group tests.

--- spec/generator/path_spec.rb
+++ spec/generator/path_spec.rb
@@ -5,7 +5,14 @@ module Xcake
     describe Path do
 
       before :each do
+          @main_group = double()
+
           @project = double()
+          allow(@project).to receive(:main_group).and_return(@main_group)
+
+          @node = double().as_null_object
+          allow(@node).to receive(:path).and_return('path')
+
           @generator = Path.new(@project)
       end
 
@@ -13,18 +20,27 @@ module Xcake
         expect(@generator.project).to be(@project)
       end
 
-      context 'when root node' do
+      context 'when finding group for root node' do
 
         it 'should return main group' do
+          allow(@node).to receive(:parent)
 
-          main_group = double()
-          allow(@project).to receive(:main_group).and_return(main_group)
-
-          node = double()
-          allow(node).to receive(:parent)
-
-          group = @generator.group_for_node(node)
+          group = @generator.group_for_node(@node)
           expect(group).to be(@project.main_group)
+        end
+      end
+
+      context 'when finding group for child node' do
+
+        it 'should return sub group' do
+
+          sub_group = double()
+
+          allow(@main_group).to receive(:find_subpath).with('path', true).and_return(sub_group)
+          allow(@node).to receive(:parent).and_return(@node)
+
+          group = @generator.group_for_node(@node)
+          expect(group).to be(sub_group)
         end
       end
 
