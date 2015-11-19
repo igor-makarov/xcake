b901ecc1fb26114f642af5bcd23612e5271fd6c2
James Campbell <james@supmenow.com>
2015-11-19 15:32:55 +0000

    Adds target build configuration tests.

--- spec/generator/target_spec.rb
+++ spec/generator/target_spec.rb
@@ -3,7 +3,20 @@ require 'spec_helper'
 module Xcake
   module Generator
     describe Target do
-      #Test Generating Targets
+
+      before :each do
+        @generator = Target.new
+      end
+
+      it 'run build configuration generator when visiting build configuration' do
+        build_configuration_generator = double()
+        allow(BuildConfiguration).to receive(:new).and_return(build_configuration_generator)
+
+        build_configuration = double()
+        expect(build_configuration).to receive(:accept).with(build_configuration_generator)
+
+        @generator.visit_buildconfiguration(build_configuration)
+      end
     end
   end
 end
