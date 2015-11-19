cb279ccd9af78cc8a99cf15ea803d311c075323a
James Campbell <james@supmenow.com>
2015-11-19 16:41:56 +0000

    Preparing tests for project resolver.

--- spec/project_structure_resolver_spec.rb
+++ spec/project_structure_resolver_spec.rb
@@ -3,5 +3,27 @@ require 'spec_helper'
 module Xcake
   describe ProjectStructureResolver do
 
+    # def visit_cakefile(cakefile)
+    #
+    #   puts "Resolving Project..."
+    #
+    #   @cakefile = cakefile
+    #
+    #   @cakefile.debug_build_configuration :debug if @cakefile.debug_build_configurations.count == 0
+    #   @cakefile.release_build_configuration :release if @cakefile.release_build_configurations.count == 0
+    # end
+    # 
+    # def visit_target(target)
+    #
+    #   puts "Resolving target #{target.name}..."
+    #
+    #   @cakefile.debug_build_configurations.each do |b|
+    #     target.debug_build_configuration(b.name)
+    #   end
+    #
+    #   @cakefile.release_build_configurations.each do |b|
+    #     target.release_build_configuration(b.name)
+    #   end
+    # end
   end
 end
