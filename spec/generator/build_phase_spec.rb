9878d5a126c2f96629a137a8950787f1f49cb690
James Campbell <james@supmenow.com>
2015-11-20 18:56:45 +0000

    Build phase implemented.

--- spec/generator/build_phase_spec.rb
+++ spec/generator/build_phase_spec.rb
@@ -4,16 +4,46 @@ module Xcake
   module Generator
     describe BuildPhase do
 
+      before :each do
+          @main_group = double()
+
+          @project = double()
+          allow(@project).to receive(:main_group).and_return(@main_group)
+
+          @node = double().as_null_object
+          @build_phase = BuildPhase.new(@project)
+      end
+
       it "should be able to install node" do
-        node = double()
-        expect(BuildPhase.can_install_node(node)).to be(true)
+        expect(BuildPhase.can_install_node(@node)).to be(true)
       end
 
       it "should store the project" do
-        project = double()
-        build_phase = BuildPhase.new(project)
+        expect(@build_phase.project).to eq(@project)
+      end
 
-        expect(build_phase.project).to eq(project)
+      context 'when finding group for root node' do
+
+        it 'should return main group' do
+          allow(@node).to receive(:parent)
+
+          group = @build_phase.group_for_node(@node)
+          expect(group).to be(@project.main_group)
+        end
+      end
+
+      context 'when finding group for child node' do
+
+        it 'should return sub group' do
+          sub_group = double()
+
+          allow(@node).to receive(:path).and_return('path')
+          allow(@main_group).to receive(:find_subpath).with('path', true).and_return(sub_group)
+          allow(@node).to receive(:parent).and_return(@node)
+
+          group = @build_phase.group_for_node(@node)
+          expect(group).to be(sub_group)
+        end
       end
     end
   end
