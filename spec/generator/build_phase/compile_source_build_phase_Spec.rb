6d03b8557821ddfde1d03e788256d156f8320589
James Campbell <james@supmenow.com>
2015-11-20 18:57:59 +0000

    All can tests in.

--- spec/generator/build_phase/compile_source_build_phase_Spec.rb
+++ spec/generator/build_phase/compile_source_build_phase_Spec.rb
@@ -4,55 +4,55 @@ module Xcake
   module Generator
     describe CompileSourceBuildPhase do
 
-      # it "should not able to install folder" do
-      #   file_reference = double()
-      #   allow(file_reference).to receive(:path) { 'Folder/' }
-      #
-      #   expect(CompileSourceBuildPhase.can_install_file_reference(file_reference)).to be(false)
-      # end
-      #
-      # it "should not able to install non source code file" do
-      #   file_reference = double()
-      #   allow(file_reference).to receive(:path) { 'File.txt' }
-      #
-      #   expect(CompileSourceBuildPhase.can_install_file_reference(file_reference)).to be(false)
-      # end
-      #
-      # it "should be able to install c source code file" do
-      #   file_reference = double()
-      #   allow(file_reference).to receive(:path) { 'File.c' }
-      #
-      #   expect(CompileSourceBuildPhase.can_install_file_reference(file_reference)).to be(true)
-      # end
-      #
-      # it "should be able to install objective-c source code file" do
-      #   file_reference = double()
-      #   allow(file_reference).to receive(:path) { 'File.m' }
-      #
-      #   expect(CompileSourceBuildPhase.can_install_file_reference(file_reference)).to be(true)
-      # end
-      #
-      # it "should be able to install objective-c++ source code file" do
-      #   file_reference = double()
-      #   allow(file_reference).to receive(:path) { 'File.mm' }
-      #
-      #   expect(CompileSourceBuildPhase.can_install_file_reference(file_reference)).to be(true)
-      # end
-      #
-      # it "should be able to install c++ source code file" do
-      #   file_reference = double()
-      #   allow(file_reference).to receive(:path) { 'File.cpp' }
-      #
-      #   expect(CompileSourceBuildPhase.can_install_file_reference(file_reference)).to be(true)
-      # end
-      #
-      # it "should be able to install swift source code file" do
-      #   file_reference = double()
-      #   allow(file_reference).to receive(:path) { 'File.swift' }
-      #
-      #   expect(CompileSourceBuildPhase.can_install_file_reference(file_reference)).to be(true)
-      # end
-      #
+      it "should not able to install folder" do
+        node = double()
+        allow(node).to receive(:path) { 'Folder/' }
+
+        expect(CompileSourceBuildPhase.can_install_node(node)).to be(false)
+      end
+
+      it "should not able to install non source code file" do
+        node = double()
+        allow(node).to receive(:path) { 'File.txt' }
+
+        expect(CompileSourceBuildPhase.can_install_node(node)).to be(false)
+      end
+
+      it "should be able to install c source code file" do
+        node = double()
+        allow(node).to receive(:path) { 'File.c' }
+
+        expect(CompileSourceBuildPhase.can_install_node(node)).to be(true)
+      end
+
+      it "should be able to install objective-c source code file" do
+        node = double()
+        allow(node).to receive(:path) { 'File.m' }
+
+        expect(CompileSourceBuildPhase.can_install_node(node)).to be(true)
+      end
+
+      it "should be able to install objective-c++ source code file" do
+        node = double()
+        allow(node).to receive(:path) { 'File.mm' }
+
+        expect(CompileSourceBuildPhase.can_install_node(node)).to be(true)
+      end
+
+      it "should be able to install c++ source code file" do
+        node = double()
+        allow(node).to receive(:path) { 'File.cpp' }
+
+        expect(CompileSourceBuildPhase.can_install_node(node)).to be(true)
+      end
+
+      it "should be able to install swift source code file" do
+        node = double()
+        allow(node).to receive(:path) { 'File.swift' }
+
+        expect(CompileSourceBuildPhase.can_install_node(node)).to be(true)
+      end
+
       # it "should add file reference to source build phase" do
       #
       #   file_reference = double()
