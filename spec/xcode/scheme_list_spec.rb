64e94a2cf816a70b04b082558efe1e3929a2ffad
James Campbell <james@supmenow.com>
2015-11-26 21:55:15 +0000

    Preparing xcode tests.

---
+++ spec/xcode/scheme_list_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+module Xcake
+  module Xcode
+    describe SchemeList do
+    end
+  end
+end
---
+++ spec/xcode/scheme_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+module Xcake
+  module Xcode
+    describe Scheme do
+    end
+  end
+end
