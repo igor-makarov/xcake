ca80bbbf26e9e4118cce68de3f0893fbc02b35f1
James Campbell <james@supmenow.com>
2015-11-29 19:14:08 +0000

    Adds sugar for supported device.

--- lib/xcake/configuration/sugar.rb
+++ lib/xcake/configuration/sugar.rb
@@ -1,8 +1,17 @@
+
+
 module Xcake
   class Configuration
 
-    def sugar
-      puts "Dayum"
+    SUPPORTED_DEVICES = {
+      :iphone_only = "1",
+      :ipad_only = "2",
+      :universal = "1,2"
+    }
+
+    def supported_devices=(devices)
+      supported_devices = SUPPORTED_DEVICES[devices]
+      settings["TARGETED_DEVICE_FAMILY"] = devices
     end
   end
 end
