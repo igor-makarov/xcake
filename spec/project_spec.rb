11398f8e1aeec99fccbd8eeea9e7e24308ce41b6
James Campbell <james@supmenow.com>
2015-12-01 23:12:14 +0000

    Removes invalid tests.

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -69,34 +69,5 @@ module Xcake
         end
       end
     end
-
-    it "should have the correct default settings" do
-      @project = Project.new
-
-      common_settings = Xcodeproj::Constants::PROJECT_DEFAULT_BUILD_SETTINGS
-      settings = Xcodeproj::Project::ProjectHelper.deep_dup(common_settings[:all])
-
-      expect(@project.default_settings).to eq(settings)
-    end
-
-    it "should have the correct default debug settings" do
-      @project = Project.new
-
-      common_settings = Xcodeproj::Constants::PROJECT_DEFAULT_BUILD_SETTINGS
-      settings = Xcodeproj::Project::ProjectHelper.deep_dup(common_settings[:all])
-      settings = settings.merge!(Xcodeproj::Project::ProjectHelper.deep_dup(common_settings[:debug]))
-
-      expect(@project.default_debug_settings).to eq(settings)
-    end
-
-    it "should have the correct default release settings" do
-      @project = Project.new
-
-      common_settings = Xcodeproj::Constants::PROJECT_DEFAULT_BUILD_SETTINGS
-      settings = Xcodeproj::Project::ProjectHelper.deep_dup(common_settings[:all])
-      settings = settings.merge!(Xcodeproj::Project::ProjectHelper.deep_dup(common_settings[:release]))
-
-      expect(@project.default_release_settings).to eq(settings)
-    end
   end
 end
