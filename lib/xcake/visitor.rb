44610fa9188ea292e30f4d2d8ff415be4953f1d6
James Campbell <james@supmenow.com>
2015-12-02 07:26:47 +0000

    DRY.

--- lib/xcake/visitor.rb
+++ lib/xcake/visitor.rb
@@ -4,23 +4,28 @@ module Xcake
     #TODO: Dry
 
     def visit(item)
-      class_name = item.class.to_s
+      item_name = item_name(item)
 
-      class_name.gsub!('Xcake::', '')
-      class_name.gsub!('::', '_')
-
-      method = "visit_#{class_name.downcase!}"
+      method = "visit_#{item_name}"
       send(method, item)
     end
 
     def leave(item)
+      item_name = item_name(item)
+
+      method = "leave_#{item_name}"
+      send(method, item)
+    end
+
+    private
+
+    def item_name(item)
       class_name = item.class.to_s
 
       class_name.gsub!('Xcake::', '')
       class_name.gsub!('::', '_')
 
-      method = "leave_#{class_name.downcase!}"
-      send(method, item)
+      class_name.downcase!
     end
   end
 end
