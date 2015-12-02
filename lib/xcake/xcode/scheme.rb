a4a2d1ba217a7f3f7f56cb6fd1d97aa3f55d4bc3
James Campbell <james@supmenow.com>
2015-12-02 11:32:17 +0000

    Name for scheme.

--- lib/xcake/xcode/scheme.rb
+++ lib/xcake/xcode/scheme.rb
@@ -3,6 +3,10 @@ require 'xcodeproj'
 module Xcake
   module Xcode
     class Scheme < Xcodeproj::XCScheme
+
+      # @return  [String] name
+      #          name for the scheme.
+      #
       attr_accessor :name
     end
   end
