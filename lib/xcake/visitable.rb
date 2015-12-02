83e0ccefd4559bdf19258b46ed37745f6be3a564
James Campbell <james@supmenow.com>
2015-12-02 07:39:04 +0000

    Visitor code commented.

--- lib/xcake/visitable.rb
+++ lib/xcake/visitable.rb
@@ -1,7 +1,22 @@
-#Create way of defining collections or properties that can be Visited
-
 module Xcake
+
+  # This namespace provides all of methods for
+  # implementing the Visitable in the  Visitor pattern.
+  #
   module Visitable
+
+    # This method accepts the visitors.
+    #
+    # The default implementatio lets the
+    # visitor visit then leave.
+    #
+    # Override this if you need to allow
+    # visitors to visit properties of a
+    # class.
+    #
+    # @param [Visitor] visitor
+    #                  the visitor to accept
+    #
     def accept(visitor)
       visitor.visit(self)
       visitor.leave(self)
--- lib/xcake/visitor.rb
+++ lib/xcake/visitor.rb
@@ -1,6 +1,45 @@
 module Xcake
+
+  # This namespace provides all of methods for
+  # implementing the Vistor in the  Visitor pattern.
+  #
+  # This implementation has a slight twist where
+  # a visitor knows when it has "left" an object
+  # it is visiting.
+  #
+  # Classes implementing these methods should
+  # add methods `visit_<visitable classname>`
+  # and `leave_<visitable classname>` for each
+  # visitable they intend to use that class with.
+  #
+  # @example Implementing Visitor Pattern
+  #
+  #          class Writer
+  #
+  #             include Visitor
+  #
+  #             def visit_page
+  #               #Write Page
+  #             end
+  #
+  #             def leave_page
+  #               #Append Footer
+  #             end
+  #          end
+  #
   module Visitor
 
+    # This is called when a visitor is visiting a
+    # visitable item.
+    #
+    # By default this method calls the method
+    # `visit_<visitable classname>` so make sure
+    # you've created a method for each visitable you
+    # intend to visit.
+    #
+    # @param [Visitable] visitable
+    #                    the visitable item the visitor is visiting
+    #
     def visit(item)
       item_name = item_name(item)
 
@@ -8,6 +47,17 @@ module Xcake
       send(method, item)
     end
 
+    # This is called when a visitor is leaving a
+    # visitable item.
+    #
+    # By default this method calls the method
+    # `leave_<visitable classname>` so make sure
+    # you've created a method for each visitable you
+    # intend to visit.
+    #
+    # @param [Visitable] visitable
+    #                    the visitable item the visitor has left
+    #
     def leave(item)
       item_name = item_name(item)
 
