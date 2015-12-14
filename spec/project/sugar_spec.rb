b03001ecae25871d442f55ec4206bbc0503cf153
James Campbell <james@supmenow.com>
2015-12-14 15:17:35 +0000

    Test outline.

--- spec/project/sugar_spec.rb
+++ spec/project/sugar_spec.rb
@@ -49,8 +49,22 @@ module Xcake
 
   context "when creating unit test target" do
     before :each do
-      @target = @project.application_for :ios, 8.0
+      @app_target = double("App Target")
+      @target = @project.unit_tests_for app_target
     end
+
+    #t.name = "#{host_target.name}Tests"
+
+    it "should set type to unit test bundle" do
+      expect(@target.type).to eq(:unit_test_bundle)
+    end
+
+    # t.platform = host_target.platform
+    # t.deployment_target = host_target.deployment_target
+    # t.language = host_target.language
+    #
+    # t.all_configurations.settings["TEST_HOST"] = "$(BUILT_PRODUCTS_DIR)/#{host_target.name}.app/#{host_target.name}"
+    # t.all_configurations.settings["BUNDLE_LOADER"] = "$(TEST_HOST)"
   end
 
   context "when creating watch target" do
