be771fa7ef754f19bd67c03af0b529a6f913d574
James Campbell <james@supmenow.com>
2015-12-15 20:54:28 +0000

    Target tests updated.

--- spec/target_spec.rb
+++ spec/target_spec.rb
@@ -2,13 +2,15 @@ require 'spec_helper'
 
 module Xcake
   describe Target do
+    before :each do
+      @target = Target.new
+    end
 
     context "when creating target" do
 
       context "for ios" do
 
         before :each do
-          @target = Target.new
           @target.platform = :ios
         end
 
@@ -20,7 +22,6 @@ module Xcake
       context "for tvos" do
 
         before :each do
-          @target = Target.new
           @target.platform = :tvos
         end
 
@@ -32,7 +33,6 @@ module Xcake
       context "for watchos" do
 
         before :each do
-          @target = Target.new
           @target.platform = :watchos
         end
 
@@ -44,7 +44,6 @@ module Xcake
       context "for osx" do
 
         before :each do
-          @target = Target.new
           @target.platform = :osx
         end
 
@@ -55,41 +54,39 @@ module Xcake
     end
 
     it "should have the correct default include files" do
-      target = Target.new
-      target.name = "test"
+      @target.name = "test"
+      expect(@target.include_files).to eq(["./test/**/*.*"])
+    end
 
-      expect(target.include_files).to eq("./test/**/*.*")
+    it "should have the correct default exclude files" do
+      expect(@target.exclude_files).to eq([])
     end
 
     it "should have the correct default debug settings" do
-      target = Target.new
+      @target.platform = :ios
+      @target.deployment_target = 8.0
+      @target.type = :application
+      @target.language = :objc
 
-      target.platform = :ios
-      target.deployment_target = 8.0
-      target.type = :application
-      target.language = :objc
-
-      settings = Xcodeproj::Project::ProjectHelper.common_build_settings(:debug, target.platform, target.deployment_target.to_s, target.type, target.language)
+      settings = Xcodeproj::Project::ProjectHelper.common_build_settings(:debug, @target.platform, @target.deployment_target.to_s, @target.type, @target.language)
       settings.merge!({
         "INFOPLIST_FILE" => "./$(PRODUCT_NAME)/Supporting Files/Info.plist"
         })
 
-      expect(target.default_debug_settings).to eq(settings)
+      expect(@target.default_debug_settings).to eq(settings)
     end
 
     it "should have the correct default release settings" do
-      target = Target.new
+      @target.platform = :ios
+      @target.deployment_target = 8.0
+      @target.type = :application
+      @target.language = :objc
 
-      target.platform = :ios
-      target.deployment_target = 8.0
-      target.type = :application
-      target.language = :objc
-
-      settings = Xcodeproj::Project::ProjectHelper.common_build_settings(:release, target.platform, target.deployment_target.to_s, target.type, target.language)
+      settings = Xcodeproj::Project::ProjectHelper.common_build_settings(:release, @target.platform, @target.deployment_target.to_s, @target.type, @target.language)
       settings.merge!({
         "INFOPLIST_FILE" => "./$(PRODUCT_NAME)/Supporting Files/Info.plist"
         })
-      expect(target.default_release_settings).to eq(settings)
+      expect(@target.default_release_settings).to eq(settings)
     end
   end
 end
