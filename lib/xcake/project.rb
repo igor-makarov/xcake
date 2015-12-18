14ac4bbce2d047daf45f81e621aa8742a7aa11b6
David Silva <davidslv@gmail.com>
2015-12-18 00:22:07 +0000

    Moves Project #accept to public area

--- lib/xcake/project.rb
+++ lib/xcake/project.rb
@@ -71,6 +71,23 @@ module Xcake
       target
     end
 
+    # Visitable
+    def accept(visitor)
+      visitor.visit(self)
+
+      flatten_configurations.each do |c|
+        visitor.visit(c)
+        visitor.leave(c)
+      end
+
+      self.targets.each do |t|
+        visitor.visit(t)
+        visitor.leave(t)
+      end
+
+      visitor.leave(self)
+    end
+
     protected
 
     # Configurable
@@ -87,25 +104,6 @@ module Xcake
     def default_release_settings
       common_settings = Xcodeproj::Constants::PROJECT_DEFAULT_BUILD_SETTINGS
       default_settings.merge!(Xcodeproj::Project::ProjectHelper.deep_dup(common_settings[:release]))
-    end
-
-    public
-
-    # Visitable
-    def accept(visitor)
-      visitor.visit(self)
-
-      flatten_configurations.each do |c|
-        visitor.visit(c)
-        visitor.leave(c)
-      end
-
-      self.targets.each do |t|
-        visitor.visit(t)
-        visitor.leave(t)
-      end
-
-      visitor.leave(self)
     end
   end
 end
