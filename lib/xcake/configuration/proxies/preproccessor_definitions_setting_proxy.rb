a6cf8cdb800978868ddf9ab0f65149d822874cdf
James Campbell <james@supmenow.com>
2015-12-23 22:24:07 +0000

    Documentation.

--- lib/xcake/configuration/proxies/preproccessor_definitions_setting_proxy.rb
+++ lib/xcake/configuration/proxies/preproccessor_definitions_setting_proxy.rb
@@ -1,16 +1,30 @@
 module Xcake
   class Configuration
+
+    # This class is a proxy to the Preprocessor build
+    # setting. It abstracts the setting of the value
+    # for this setting and makes it easier to define
+    # new preprocessor definitions.
+    #
     class PreprocessorDefinitionsSettingProxy
 
-      # Document
+      # the settings for the configuration this
+      # proxy is for
+      #
+      # @return [Hash<String, Object>] the settings for the proxied configuration
       #
       attr_accessor :settings
 
-      # Document
+      # @return [String] the key for the settings where the preprocessor definitions are kept
       #
       attr_accessor :key
 
-      # Document
+      # @param    [Hash<String, Object>] settings
+      #           the settings for the proxied configuration
+      #
+      # @param    [String] key
+      #           the key for the settings where the preprocessor
+      #           definitions are kept
       #
       def initialize(settings, key)
         @settings = settings
@@ -18,7 +32,13 @@ module Xcake
         @settings[@key] ||= ["$(inherited)"]
       end
 
-      # Document
+      # The subscript operator is used to define the preprocessor defination
+      #
+      # @param    [String] key
+      #           the key for the preprocessor definition
+      #
+      # @param    [Object] value
+      #           the value for the preprocessor definition
       #
       def []=(key, value)
         @settings[@key] << "#{key}=#{value}"
