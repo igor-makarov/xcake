7a195c7d4db9ba881a4c30ee9e473582df4399f9
James Campbell <james@supmenow.com>
2016-01-16 14:23:28 +0000

    Fixes spacing.

--- lib/xcake/command/init.rb
+++ lib/xcake/command/init.rb
@@ -1,12 +1,10 @@
 module Xcake
   class Command
     class Init < Command
-
       self.summary = "Initilises an example Cakefile for creating a project"
       self.description = "Initilises an example Cakefile for creating a project"
 
       def run
-
         cakefile_path = "#{File.dirname(__FILE__)}/../resources/Cakefile"
         cakefile_contents = File.read(cakefile_path)
         File.write("Cakefile", cakefile_contents)
