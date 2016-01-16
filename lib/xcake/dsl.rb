944509d18fc6a0e5031c920bd934957370170053
James Campbell <james@supmenow.com>
2016-01-16 14:26:17 +0000

    Implementing simple DSL.

--- lib/xcake/dsl.rb
+++ lib/xcake/dsl.rb
@@ -2,5 +2,17 @@ module xcake
   class DSL
     def project
     end
+
+    def target
+    end
+
+    def application
+    end
+
+    def watch
+    end
+
+    def configuration
+    end
   end
 end
