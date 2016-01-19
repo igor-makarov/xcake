aec4f63a6388c782fcc1546c609ead3bde626425
James Campbell <james@supmenow.com>
2016-01-19 12:35:05 +0000

    Prepare schemes.

---
+++ lib/xcake/generator/scheme.rb
@@ -0,0 +1,8 @@
+require 'xcodeproj'
+
+module Xcake
+  module Generator
+    class Scheme
+    end
+  end
+end
---
+++ lib/xcake/generator/target_dependency.rb
@@ -0,0 +1,8 @@
+require 'xcodeproj'
+
+module Xcake
+  module Generator
+    class TargetDependency
+    end
+  end
+end
