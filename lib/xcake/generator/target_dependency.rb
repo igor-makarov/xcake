10118f3b9855219a8a2cb5229a55669042cbedd0
James Campbell <james@supmenow.com>
2016-01-19 15:08:28 +0000

    Target table.

--- lib/xcake/generator/target_dependency.rb
+++ lib/xcake/generator/target_dependency.rb
@@ -14,13 +14,13 @@ module Xcake
 
       def visit_target(target)
 
-        native_target = context.native_target_for(target)
+        native_target = context.target_table[target]
 
         target.target_dependancies.each do |dep|
 
           puts "Adding #{dep} as dependency for #{target}..."
 
-          native_dependancy = context.native_target_for(dep)
+          native_dependancy = context.target_table[dep]
           native_target.add_dependency(native_dependancy)
         end
       end
