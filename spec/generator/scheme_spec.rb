bca7079f43ec93b02d6c14cf7105e2b3bcf8cfc7
James Campbell <james@supmenow.com>
2016-01-19 18:55:02 +0000

    Updating references.

--- spec/generator/scheme_spec.rb
+++ spec/generator/scheme_spec.rb
@@ -6,7 +6,7 @@ module Xcake
 
       before :each do
         @context = double("Context").as_null_object
-        @scheme_list = Scheme.new(@context)
+        @generator = Scheme.new(@context)
 
         @target = double("Target").as_null_object
         allow(@target).to receive(:name).and_return("app")
@@ -17,28 +17,23 @@ module Xcake
       end
 
       it "should store project" do
-        expect(@scheme_list.project).to be(@project)
+        expect(@generator.project).to be(@project)
       end
 
       it "should create empty schemes array" do
-        expect(@scheme_list.schemes).to eq([])
+        expect(@generator.schemes).to eq([])
       end
 
       it "should create default scheme management hash" do
-        expect(@scheme_list.xcschememanagement).to eq({
+        expect(@generator.xcschememanagement).to eq({
           'SchemeUserState' => {},
           'SuppressBuildableAutocreation' => {}
         })
       end
 
-      it "should create schemes for each target" do
-        expect(@scheme_list).to receive(:create_schemes_for_target).with(@target)
-        @scheme_list.recreate_schemes
-      end
-
       it "should create scheme for application" do
-        expect(@scheme_list).to receive(:create_schemes_for_application).with(@target)
-        @scheme_list.create_schemes_for_target(@target)
+        expect(@generator).to receive(:create_schemes_for_application).with(@target)
+        @generator.create_schemes_for_target(@target)
       end
 
       context "when creating scheme for application" do
@@ -54,23 +49,23 @@ module Xcake
         it "should set correct name" do
           allow(@build_configuration).to receive(:name).and_return("debug")
           expect(@scheme).to receive(:name=).with("#{@target.name}-#{@build_configuration.name}")
-          @scheme_list.create_schemes_for_application(@target)
+          @generator.create_schemes_for_application(@target)
         end
 
         it "should configure with build target" do
           expect(@scheme).to receive(:configure_with_targets).with(@target, nil)
-          @scheme_list.create_schemes_for_application(@target)
+          @generator.create_schemes_for_application(@target)
         end
 
         it "should suppress target scheme autocreation" do
-          @scheme_list.create_schemes_for_application(@target)
-          autocreation_setting = @scheme_list.xcschememanagement['SuppressBuildableAutocreation'][@target.uuid]['primary']
+          @generator.create_schemes_for_application(@target)
+          autocreation_setting = @generator.xcschememanagement['SuppressBuildableAutocreation'][@target.uuid]['primary']
           expect(autocreation_setting).to eq(true)
         end
 
         it "should store scheme" do
-          @scheme_list.create_schemes_for_application(@target)
-          expect(@scheme_list.schemes.count).to eq(1)
+          @generator.create_schemes_for_application(@target)
+          expect(@generator.schemes.count).to eq(1)
         end
 
         context "and adding unit test" do
@@ -82,17 +77,17 @@ module Xcake
 
           it "should configure with test target" do
             expect(@scheme).to receive(:configure_with_targets).with(@target, @unit_test_target)
-            @scheme_list.create_schemes_for_application(@target)
+            @generator.create_schemes_for_application(@target)
           end
 
           it "add target as depedancy for unit test target" do
             expect(@unit_test_target).to receive(:add_dependency).with(@target)
-            @scheme_list.create_schemes_for_application(@target)
+            @generator.create_schemes_for_application(@target)
           end
 
           it "should suppress unit test target scheme autocreation" do
-            @scheme_list.create_schemes_for_application(@target)
-            autocreation_setting = @scheme_list.xcschememanagement['SuppressBuildableAutocreation'][@unit_test_target.uuid]['primary']
+            @generator.create_schemes_for_application(@target)
+            autocreation_setting = @generator.xcschememanagement['SuppressBuildableAutocreation'][@unit_test_target.uuid]['primary']
             expect(autocreation_setting).to eq(true)
           end
         end
@@ -106,24 +101,24 @@ module Xcake
           allow(Xcodeproj).to receive(:write_plist)
           expect(FileUtils).to receive(:mkdir_p).with(schemes_dir)
 
-          @scheme_list.save(".")
+          @generator.save(".")
         end
 
         context "schemes" do
 
           before :each do
             @scheme = double("Scheme").as_null_object
-            allow(@scheme_list).to receive(:schemes).and_return([@scheme])
+            allow(@generator).to receive(:schemes).and_return([@scheme])
           end
 
           it "should save scheme" do
             expect(@scheme).to receive(:save_as).with(@project.path, @scheme.name, true)
-            @scheme_list.save(".")
+            @generator.save(".")
           end
 
           it "should add scheme to scheme managment list" do
-            @scheme_list.save(".")
-            scheme_entry = @scheme_list.xcschememanagement['SchemeUserState']["#{@scheme.name}.xcscheme_^#shared#^_"]
+            @generator.save(".")
+            scheme_entry = @generator.xcschememanagement['SchemeUserState']["#{@scheme.name}.xcscheme_^#shared#^_"]
 
             expect(scheme_entry).to eq({
               "isShown" => true
@@ -135,9 +130,9 @@ module Xcake
 
           it "should be saved" do
             xcschememanagement_path = Scheme.user_data_dir(".") + 'xcschememanagement.plist'
-            expect(Xcodeproj).to receive(:write_plist).with(@scheme_list.xcschememanagement, xcschememanagement_path)
+            expect(Xcodeproj).to receive(:write_plist).with(@generator.xcschememanagement, xcschememanagement_path)
 
-            @scheme_list.save(".")
+            @generator.save(".")
           end
         end
       end
