a50a1c3da6580d677d79cd2858158f50460ab74d
James Campbell <james@supmenow.com>
2016-01-19 14:48:25 +0000

    Implemented target depency outline.

--- lib/xcake/generator/target_dependency.rb
+++ lib/xcake/generator/target_dependency.rb
@@ -13,6 +13,13 @@ module Xcake
       end
 
       def visit_target(target)
+
+        native_target = context.native_target_for(target)
+
+        target.target_dependancies.each do |dep|
+          native_dependancy = context.native_target_for(dep)
+          native_target.add_dependency(native_dependancy)
+        end
       end
 
       def leave_target(target)
