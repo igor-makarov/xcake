95bd5592ff4c87094bde442f562165856d867f56
James Campbell <james@supmenow.com>
2016-01-19 14:46:14 +0000

    Implement target dependency handling.

--- lib/xcake/generator/target_dependency.rb
+++ lib/xcake/generator/target_dependency.rb
@@ -4,10 +4,18 @@ module Xcake
   module Generator
     class TargetDependency
 
+      include Visitor
+
       attr_accessor :context
 
       def initialize(context)
         @context = context
+      end
+
+      def visit_target(target)
+      end
+
+      def leave_target(target)
       end
     end
   end
