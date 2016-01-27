7424b5b0912bf3902f5d0df0a444c43d93592429
James Campbell <james@supmenow.com>
2016-01-27 20:20:31 +0000

    Removes properties.

--- lib/xcake/generator/context.rb
+++ lib/xcake/generator/context.rb
@@ -3,12 +3,6 @@ require 'xcodeproj'
 module Xcake
   module Generator
     class Context
-      include Visitable
-
-      attr_accessor :project
-      attr_accessor :root_node
-      attr_accessor :target_table
-
     end
   end
 end
