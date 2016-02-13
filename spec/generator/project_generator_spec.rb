cb7a2658cef49466140e56c43216584a00cd7e4d
James Campbell <james@supmenow.com>
2016-02-13 09:05:18 +0000

    Removes og.

--- spec/generator/project_generator_spec.rb
+++ spec/generator/project_generator_spec.rb
@@ -4,8 +4,6 @@ module Xcake
   describe ProjectGenerator do
     before :each do
       @dsl_project = double("DSL Project").as_null_object
-      allow(@dsl_project).to receive(:class_prefix).and_return("Prefix")
-      allow(@dsl_project).to receive(:organization).and_return("Organization")
 
       @project = double("Project").as_null_object
 
@@ -13,16 +11,6 @@ module Xcake
       allow(@context).to receive(:native_object_for).and_return(@project)
 
       @generator = ProjectGenerator.new(@context)
-    end
-
-    it "should set the class prefix" do
-      expect(@project).to receive(:class_prefix=).with(@dsl_project.class_prefix)
-      @generator.visit_project(@dsl_project)
-    end
-
-    it "should set the organization" do
-      expect(@project).to receive(:organization=).with(@dsl_project.organization)
-      @generator.visit_project(@dsl_project)
     end
 
     it "should save the project" do
