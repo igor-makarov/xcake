a21a60c30313eb221121684ccd27af6aea2f802f
James Campbell <james@supmenow.com>
2016-02-13 09:30:55 +0000

    Removes spacing.

--- lib/xcake/generator/target_dependency_generator.rb
+++ lib/xcake/generator/target_dependency_generator.rb
@@ -1,12 +1,10 @@
 module Xcake
   class TargetDependencyGenerator < Generator
-
     def self.dependencies
       [TargetGenerator]
     end
 
     def visit_target(target)
-
       native_target = @context.native_object_for(target)
       native_target.dependencies = target.target_dependencies.map do |dep|
         @context.native_object_for(dep)
