f2c9d31cedbbbb2369a1b16a077b671a47e6ee13
James Campbell <james@supmenow.com>
2016-02-13 08:43:44 +0000

    Adds in proper watch target inference.

--- lib/xcake/generator/target_build_phase_generator.rb
+++ lib/xcake/generator/target_build_phase_generator.rb
@@ -14,6 +14,30 @@ module Xcake
 
       native_target = @context.native_object_for(target)
 
+      target.target_dependencies.each do |dep|
+
+        native_dep = @context.native_object_for(dep)
+
+        case dep.type
+        when :watch2_app
+          create_embed_watchapp_phase(native_target, native_dep)
+        when :watch2_extension
+          create_embed_watchapp_phase(native_target, native_dep)
+        end
+      end
+    end
+
+    def create_embed_watchapp_phase(native_target, native_watchapp_target)
+      phase = target.new_copy_files_build_phase("Embed Watch Content")
+      phase.add_file_reference(native_watchapp_target.product_reference)
+      phase
+    end
+
+    def create_embed_watchapp_extension_phase(native_target, native_watchapp_extension_target)
+      phase = target.new_copy_files_build_phase("Embed App Extensions")
+      phase.dstPath = "$(CONTENTS_FOLDER_PATH)/Watch"
+      phase.add_file_reference(native_watchapp_extension_target.product_reference)
+      phase
     end
   end
 end
