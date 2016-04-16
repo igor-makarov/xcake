1a8282dec8a82123463a555a52c213f14da017e7
James Campbell <james@supmenow.com>
2016-04-16 16:58:43 +0100

    Implements @colinta's XCConfig support.

--- lib/xcake/generator/configuration_generator.rb
+++ lib/xcake/generator/configuration_generator.rb
@@ -28,14 +28,17 @@ module Xcake
         build_configuration.name = configuration.name
         build_configuration.build_settings = configuration.settings
 
- #      Move this to an installer:
- #      group = group_for_node(node)
- #      file_reference = group.new_reference(node.path)
- #      @native_configuration.base_configuration_reference = file_reference
- #      build_configuration.base_configuration_reference = configuration.configuration_file
-
         native_configuration_object = @context.native_object_for(configuration_object)
         native_configuration_object.build_configurations << build_configuration
+
+        #TODO: BDD This.
+        #TODO: Remove need to construct a Node Object.
+        #TODO: Move this into its own generator.
+        node = Node.new
+        node.path = configuration.configuration_file
+        native_group = @context.native_object_for(node)
+        file_reference = native_group.new_reference(node.path)
+        native_configuration_object.base_configuration_reference = file_reference
       end
     end
   end
