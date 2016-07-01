466b4f6ef61d9c85c4aaf5f3d7ecfa656fdb7c19
James Campbell <james@supmenow.com>
2016-07-01 16:12:33 +0100

    Simplify generatior code.

--- lib/xcake/generator/target_library_generator.rb
+++ lib/xcake/generator/target_library_generator.rb
@@ -8,9 +8,9 @@ module Xcake
       puts "Integrating System Libraries for #{target}..."
 
       native_target = @context.native_object_for(target)
-      if target.system_libraries
-        native_target.add_system_libraries(target.system_libraries)
-      end
+
+      system_libraries = target.system_libraries
+      native_target.add_system_libraries(system_libraries) if system_libraries
     end
   end
 end
