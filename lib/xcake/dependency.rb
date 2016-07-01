cd29d17aea149f9750b2c2d06a4a27eee0217b3c
James Campbell <james@supmenow.com>
2016-07-01 09:33:49 +0100

    Simplify dependency.

--- lib/xcake/dependency.rb
+++ lib/xcake/dependency.rb
@@ -1,24 +1,16 @@
 require 'xcodeproj'
 
 #TODO: Figure out a more robust Dependency system
-# - Needs to handle subclass of subclass.....
-# - Needs to have better mixin hiearchy
 # - Implement a pre-dependency (Something that this needs to happen in front of)
 
 module Xcake
   module Dependency
-    module ClassMethods
-      def name
-        self
-      end
-
-      def dependencies
-        []
-      end
+    def self.name
+      self
     end
 
-    def self.included(klass)
-      klass.extend ClassMethods  # Somewhat controversial
+    def self.dependencies
+      []
     end
   end
 end
