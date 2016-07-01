db8c47e2b94b818ae91ffab1ee177d1dc3ebd07f
James Campbell <james@supmenow.com>
2016-07-01 09:54:51 +0100

    Preparing for plugin tests.

---
+++ spec/plugin_spec.rb
@@ -0,0 +1,26 @@
+require "spec_helper"
+
+module Xcake
+  describe Plugin do
+    # context "when created" do
+    #   before :each do
+    #     @repository = double("Repository")
+    #     @dependency = double("Dependency")
+    #     @provider = DependencyProvider.new(@repository)
+    #   end
+    #
+    #   it "should set the repository" do
+    #     expect(@provider.repository).to eq(@repository)
+    #   end
+    #
+    #   it "should provide dependencies for specification" do
+    #     specification = double("Specification")
+    #     dependencies = []
+    #     allow(specification).to receive(:dependencies).and_return(dependencies)
+    #
+    #     result = @provider.dependencies_for(specification)
+    #     expect(result).to eq(dependencies)
+    #   end
+    # end
+  end
+end
