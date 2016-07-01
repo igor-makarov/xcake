cc0b415a89f36e3b5d912aadd52ee951ddbc75da
James Campbell <james@supmenow.com>
2016-07-01 10:20:10 +0100

    Preparing for new tests for Plugin and core extensions.

---
+++ spec/core_ext/string_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+
+describe String do
+#   it "should provide dependencies for specification" do
+#     @repository = double("Repository")
+#     @dependency = double("Dependency")
+#     @provider = DependencyProvider.new(@repository)
+#     specification = double("Specification")
+#     dependencies = []
+#     allow(specification).to receive(:dependencies).and_return(dependencies)
+#
+#     result = @provider.dependencies_for(specification)
+#     expect(result).to eq(dependencies)
+#   end
+end
--- spec/plugin_spec.rb
+++ spec/plugin_spec.rb
@@ -2,25 +2,16 @@ require "spec_helper"
 
 module Xcake
   describe Plugin do
-    # context "when created" do
-    #   before :each do
-    #     @repository = double("Repository")
-    #     @dependency = double("Dependency")
-    #     @provider = DependencyProvider.new(@repository)
-    #   end
+    # it "should provide dependencies for specification" do
+    #   @repository = double("Repository")
+    #   @dependency = double("Dependency")
+    #   @provider = DependencyProvider.new(@repository)
+    #   specification = double("Specification")
+    #   dependencies = []
+    #   allow(specification).to receive(:dependencies).and_return(dependencies)
     #
-    #   it "should set the repository" do
-    #     expect(@provider.repository).to eq(@repository)
-    #   end
-    #
-    #   it "should provide dependencies for specification" do
-    #     specification = double("Specification")
-    #     dependencies = []
-    #     allow(specification).to receive(:dependencies).and_return(dependencies)
-    #
-    #     result = @provider.dependencies_for(specification)
-    #     expect(result).to eq(dependencies)
-    #   end
+    #   result = @provider.dependencies_for(specification)
+    #   expect(result).to eq(dependencies)
     # end
   end
 end
