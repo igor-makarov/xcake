6f656cbfcf0ecbc46515da593a467b7d4f53724e
James Campbell <james@supmenow.com>
2016-07-01 10:04:44 +0100

    Updates code for dependencies.

--- lib/xcake/dependency.rb
+++ lib/xcake/dependency.rb
@@ -1,8 +1,5 @@
 require 'xcodeproj'
 
-#TODO: Figure out a more robust Dependency system
-# - Implement a pre-dependency (Something that this needs to happen in front of)
-
 module Xcake
   module Dependency
 
