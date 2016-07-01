40e694e482ed296c6e852e885a2ee8032e6f2613
James Campbell <james@supmenow.com>
2016-07-01 11:47:51 +0100

    Updates based on Hound CI.

--- spec/core_ext/array_spec.rb
+++ spec/core_ext/array_spec.rb
@@ -2,6 +2,6 @@ require "spec_helper"
 
 describe Array do
   it "should convert to hash" do
-    expect(["key", "value"].to_h).to eq({ "key" => "value" })
+    expect(%w(key value).to_h).to eq("key" => "value")
   end
 end
