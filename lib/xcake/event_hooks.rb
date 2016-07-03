6efb24e0931e49647b75165d095063a43e1e99c7
James Campbell <james@supmenow.com>
2016-07-03 18:36:06 +0100

    Use exception.

--- lib/xcake/event_hooks.rb
+++ lib/xcake/event_hooks.rb
@@ -7,5 +7,9 @@ module Xcake
     # Defines hook which is ran after a Cakefile is initilized.
     #
     define_hooks :after_cakefile_init
+
+    # Defines hook which is ran after an exception
+    #
+    define_hooks :after_exception
   end
 end
--- lib/xcake/ui.rb
+++ lib/xcake/ui.rb
@@ -1,12 +1,18 @@
 require 'cork'
 
-# TODO: i18n this.
-board = Cork::Board.new()
+module Xcake
+  class UI
+    def initialize
+      # TODO: i18n this.
+      board = Cork::Board.new()
 
-Xcake::EventHooks.after_cakefile_init do
-  board.notice('Open Cakefile to edit and run xcake make to get your xcode project')
-end
+      Xcake::EventHooks.after_cakefile_init do
+        board.notice('Open Cakefile to edit and run xcake make to get your xcode project')
+      end
 
-Xcake::EventHooks.after_cakefile_init do
-  board.notice('Open Cakefile to edit and run xcake make to get your xcode project')
+      Xcake::EventHooks.after_exception do
+        board.warn('An Error Occured')
+      end
+    end
+  end
 end
