6a72e7384993f27a8389358b79ae7a991a4d898d
James Campbell <james@supmenow.com>
2016-07-09 15:44:57 +0100

    Implement tests.

--- spec/path_classifier_spec.rb
+++ spec/path_classifier_spec.rb
@@ -7,6 +7,31 @@ module Xcake
         @context = double('Context')
         @installer = PathClassifier.new
       end
+
+      it 'should include files' do
+        should_install = PathClassifier.should_include_path('./File.txt')
+        expect(should_install).to be(true)
+      end
+
+      it 'should include folders' do
+        should_install = PathClassifier.should_include_path('./Folder')
+        expect(should_install).to be(true)
+      end
+
+      it 'should not include folders for localization' do
+        should_install = PathClassifier.should_include_path('./Folder.lproj')
+        expect(should_install).to be(false)
+      end
+
+      it 'should include localized files' do
+        should_install = PathClassifier.should_include_path('./Folder.lproj/File.txt')
+        expect(should_install).to be(true)
+      end
+
+      it 'should include files inside of a classified container' do
+        should_install = PathClassifier.should_include_path('./Folder.framework/File.txt')
+        expect(should_install).to be(false)
+      end
     end
   end
 end
