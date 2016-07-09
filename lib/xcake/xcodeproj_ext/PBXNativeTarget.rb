058b6bfc59ad4208f91da3b95c52aa8b8c8d78fd
James Campbell <james@supmenow.com>
2016-07-09 21:47:02 +0100

    BDD.

--- lib/xcake/xcodeproj_ext/PBXNativeTarget.rb
+++ lib/xcake/xcodeproj_ext/PBXNativeTarget.rb
@@ -4,6 +4,7 @@ module Xcodeproj
   class Project
     module Object
       class PBXNativeTarget
+        #TODO: BDD
         def build_phase_by_class(phase_class)
           find_or_create_build_phase_by_class(phase_class)
         end
