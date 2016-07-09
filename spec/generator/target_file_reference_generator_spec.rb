4c5cb2a9327b2d9b6f45be46f41fe387b873526e
James Campbell <james@supmenow.com>
2016-07-09 15:29:44 +0100

    Fixes normlization.

--- spec/generator/target_file_reference_generator_spec.rb
+++ spec/generator/target_file_reference_generator_spec.rb
@@ -1,6 +1,5 @@
 require 'spec_helper'
 
-# TODO: How do we stub file system
 module Xcake
   describe TargetFileReferenceGenerator do
     before :each do
@@ -28,6 +27,21 @@ module Xcake
     it 'should ignore excluded paths' do
       allow(@target).to receive(:include_files).and_return(@paths)
       allow(@target).to receive(:exclude_files).and_return(@paths)
+
+      path = @paths.first
+      expect(@context).to_not receive(:file_reference_for_path).with(path)
+      @generator.visit_target(@target)
+    end
+
+    it 'should ignore paths with different root specifier' do
+
+      exclude_paths = [
+          './'
+      ]
+
+      allow(@target).to receive(:include_files).and_return(@paths)
+      allow(@target).to receive(:exclude_files).and_return(exclude_paths)
+      allow(Dir).to receive(:glob).with(exclude_paths).and_return(exclude_paths)
 
       path = @paths.first
       expect(@context).to_not receive(:file_reference_for_path).with(path)
