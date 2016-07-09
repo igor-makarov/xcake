94e6326948592667a0a234fa53e1e7dfbdb9ba1b
James Campbell <james@supmenow.com>
2016-07-09 15:26:47 +0100

    Removes tests.

--- spec/path_classifier_spec.rb
+++ spec/path_classifier_spec.rb
@@ -7,16 +7,6 @@ module Xcake
         @context = double('Context')
         @installer = PathClassifier.new
       end
-
-      it 'should not be able to classify a path' do
-        can_classify = PathClassifier.can_classify_path("")
-        expect(can_classify).to be false
-      end
-
-      it 'should not include any path' do
-        should_include = PathClassifier.should_include_path("")
-        expect(should_include).to be false
-      end
     end
   end
 end
