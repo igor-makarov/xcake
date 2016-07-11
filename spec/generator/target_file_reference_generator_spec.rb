29b8a0c3244aba460089741abcc30fc4e9a35788
James Campbell <james@supmenow.com>
2016-07-11 19:01:20 +0100

    All existing tests passing.

--- spec/generator/target_file_reference_generator_spec.rb
+++ spec/generator/target_file_reference_generator_spec.rb
@@ -5,6 +5,8 @@ module Xcake
     before :each do
       @context = double('Context')
       @target = double('Target')
+      @native_target = double('Native Target').as_null_object
+
       @generator = TargetFileReferenceGenerator.new(@context)
 
       @paths = [
@@ -13,6 +15,7 @@ module Xcake
 
       allow(Dir).to receive(:glob).with([]).and_return([])
       allow(Dir).to receive(:glob).with(@paths).and_return(@paths)
+      allow(@context).to receive(:native_object_for).with(@target).and_return(@native_target)
     end
 
     it 'should add included paths' do
@@ -20,6 +23,7 @@ module Xcake
       allow(@target).to receive(:exclude_files).and_return([])
 
       path = @paths.first
+
       expect(@context).to receive(:file_reference_for_path).with(path)
       @generator.visit_target(@target)
     end
