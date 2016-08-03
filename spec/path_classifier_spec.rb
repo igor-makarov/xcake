cf01053035a0bb7adca3b1475d913c64d2a542b7
James Campbell <jscampbell.05@googlemail.com>
2016-08-03 09:30:43 +0100

    Copy headers build phase can only be explicit. (#100)
    * All tests passing.
    
    * All tests passing.

--- spec/path_classifier_spec.rb
+++ spec/path_classifier_spec.rb
@@ -2,31 +2,25 @@ require 'spec_helper'
 
 module Xcake
   describe PathClassifier do
-    context 'when created' do
-      it 'should include files' do
-        should_install = PathClassifier.should_include_path?('./File.txt')
-        expect(should_install).to be(true)
-      end
 
-      it 'should include folders' do
-        should_install = PathClassifier.should_include_path?('./Folder')
-        expect(should_install).to be(true)
-      end
+    it 'should allow PBXResourcesBuildPhase to be implicity created' do
+      should_create = PathClassifier.should_create_build_phase_for_classification?(:PBXResourcesBuildPhase)
+      expect(should_create).to be(true)
+    end
 
-      it 'should not include folders for localization' do
-        should_install = PathClassifier.should_include_path?('./Folder.lproj')
-        expect(should_install).to be(false)
-      end
+    it 'should allow PBXFrameworksBuildPhase to be implicity created' do
+      should_create = PathClassifier.should_create_build_phase_for_classification?(:PBXFrameworksBuildPhase)
+      expect(should_create).to be(true)
+    end
 
-      it 'should include localized files' do
-        should_install = PathClassifier.should_include_path?('./Folder.lproj/File.txt')
-        expect(should_install).to be(true)
-      end
+    it 'should allow PBXSourcesBuildPhase to be implicity created' do
+      should_create = PathClassifier.should_create_build_phase_for_classification?(:PBXSourcesBuildPhase)
+      expect(should_create).to be(true)
+    end
 
-      it 'should not include files inside of a classified container' do
-        should_install = PathClassifier.should_include_path?('./Folder.framework/File.txt')
-        expect(should_install).to be(false)
-      end
+    it 'shouldn\'t allow PBXHeadersBuildPhase to be implicity created' do
+      should_create = PathClassifier.should_create_build_phase_for_classification?(:PBXHeadersBuildPhase)
+      expect(should_create).to be(false)
     end
 
     context 'for a framework' do
@@ -196,6 +190,18 @@ module Xcake
 
       it 'should classify as PBXResourcesBuildPhase' do
         classification = PathClassifier.classification_for_path('./Assets.xcassets')
+        expect(classification).to be(:PBXResourcesBuildPhase)
+      end
+    end
+
+    context 'for resource file' do
+      it 'should include file' do
+        should_install = PathClassifier.should_include_path?('./Image.png')
+        expect(should_install).to be(true)
+      end
+
+      it 'should classify as PBXResourcesBuildPhase' do
+        classification = PathClassifier.classification_for_path('./Image.png')
         expect(classification).to be(:PBXResourcesBuildPhase)
       end
     end
