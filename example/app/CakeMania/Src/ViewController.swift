7d1fae140cb396f865245560664ba47b404d6dcd
Maxim Khatskevich <maxim@khatskevi.ch>
2016-11-25 01:40:17 +0500

    Added example app project (instead of just sample Cakefile)
    Closed https://github.com/jcampbell05/xcake/issues/118.
    + minor update in ReadMe file.

---
+++ example/app/CakeMania/Src/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CakeMania
+//
+//  Created by Maxim Khatskevich on 11/25/16.
+//  Copyright © 2016 CakeMania Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
