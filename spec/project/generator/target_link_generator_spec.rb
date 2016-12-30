365eca28df90d1e38e58e6d3db991ccda526e92c
James Campbell <james@supmenow.com>
2016-12-30 20:05:45 +0000

    Target linking unit tested.

--- spec/project/generator/target_link_generator_spec.rb
+++ spec/project/generator/target_link_generator_spec.rb
@@ -3,39 +3,38 @@ require 'spec_helper'
 module Xcake
   describe TargetLinkGenerator do
     before :each do
-      
-      link_build_phase_phase = double('Link Build Phase')
-      @link_build_phase_files = []
-      allow(link_build_phase).to receive(:files).and_return(@link_build_phase_files)
+      @linked_product_reference = double('Linked Product Reference')
+      @frameworks_build_phase = double('Frameworks Build Phase')
 
-      @linked_path = "LinkedPath.framework"
-      linked_product_reference = double('Linked Product Reference')
-      allow(linked_product_reference).to receive(:path).and_return(@linked_path)
+      native_target = double('Native Target').as_null_object
+      allow(native_target).to receive(:frameworks_build_phase).and_return(@frameworks_build_phase)
 
-      native_target = double('Target').as_null_object
-      allow(native_target).to receive(:product_reference).and_return(linked_product_reference)
+      linked_native_target = double('Linked Native Target').as_null_object
+      allow(linked_native_target).to receive(:product_reference).and_return(@linked_product_reference)
 
       @linked_target = double('Linked Target')
-      context = double('Context')
-      allow(context).to receive(:native_object_for).with(@linked_target).and_return(native_target)
 
       @target = double('Target')
       @target_dependencies = []
       allow(@target).to receive(:linked_targets).and_return([@linked_target])
-      allow(@target).to receive(:link_build_phase).and_return(link_build_phase)
       allow(@target).to receive(:target_dependencies).and_return(@target_dependencies)
+    
+      context = double('Context')
+      allow(context).to receive(:native_object_for).with(@target).and_return(native_target)
+      allow(context).to receive(:native_object_for).with(@linked_target).and_return(linked_native_target)
       
       @generator = TargetLinkGenerator.new(context)
     end
 
     it 'should add dependency to linked target' do
+      allow(@frameworks_build_phase).to receive(:add_file_reference)
       @generator.visit_target(@target)
       expect(@target_dependencies).to eq([@linked_target])
     end
 
     it 'should add linked target product reference to link library build phase'  do
+      expect(@frameworks_build_phase).to receive(:add_file_reference).with(@linked_product_reference)
       @generator.visit_target(@target)
-      expect(@link_build_phase_files).to eq([@linked_path])
     end
   end
 end
