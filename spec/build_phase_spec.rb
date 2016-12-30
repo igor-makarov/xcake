e02c97508d9e269700477c57e11640d880525af9
James Campbell <james@supmenow.com>
2016-12-30 15:52:42 +0000

    Adds test for rasing error.

--- spec/build_phase_spec.rb
+++ spec/build_phase_spec.rb
@@ -2,14 +2,21 @@ require 'spec_helper'
 
 module Xcake
   describe BuildPhase do
+
+    before :each do
+      @phase = BuildPhase.new
+    end
     
+    it 'should raise error when accessing build phase type' do
+        expect { @phase.build_phase_type }.to raise_error
+    end
+
     it 'should set name when configuring native build phase' do
         native_build_phase = double('Native Build Phase')
-        build_phase = BuildPhase.new
-        build_phase.name = "Name"
+        @phase.name = "Name"
 
-        expect(native_build_phase).to receive(:name=).with(build_phase.name)
-        build_phase.configure_native_build_phase(native_build_phase, nil)
+        expect(native_build_phase).to receive(:name=).with(@phase.name)
+        @phase.configure_native_build_phase(native_build_phase, nil)
     end
   end
 end
