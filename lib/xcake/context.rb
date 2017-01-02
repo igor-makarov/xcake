5478ce3bb4c9b9600d414c9ddc706ece7872fe43
James Campbell <james@supmenow.com>
2017-01-02 15:45:27 +0000

    Context documented.

--- lib/xcake/context.rb
+++ lib/xcake/context.rb
@@ -1,20 +1,42 @@
 require 'xcodeproj'
 
 module Xcake
+  # The Context is an object that tracking of native objects in the project
+  # format.
+  #
+  # It allows components of the Xcake generator system to retrieve
+  # native representation of a DSL object which is useful for operations
+  # which require multiple stepas.
+  #
+  # See XcodeXcodeprojContext and the Genrators for how this is used.
+  #
   module Context
+
+    # The internal hash of native objects reference by their
+    # related DSL object
+    #
     attr_accessor :object_hash
 
     def object_hash
       @object_hash ||= {}
     end
 
+    # This method returns or creates a native object
+    # for a DSL object
+    #
     def native_object_for(dsl_object)
       object_hash[dsl_object] ||= create_object_for(dsl_object)
     end
 
+    # This method returns or creates a file reference
+    # for a path
+    #
     def file_reference_for_path(path)
     end
 
+    # This method returns or creates a scheme list for
+    # the project
+    #
     def scheme_list
     end
   end
