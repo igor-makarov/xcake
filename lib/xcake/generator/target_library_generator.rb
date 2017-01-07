edc4769cc46e550686c1ea577d2dfd41ae5e1959
James Campbell <james@supmenow.com>
2017-01-07 10:33:56 +0000

    Don't show system framework integration it's noise.

--- lib/xcake/generator/target_library_generator.rb
+++ lib/xcake/generator/target_library_generator.rb
@@ -5,12 +5,13 @@ module Xcake
     end
 
     def visit_target(target)
-      unless target.system_libraries.nil?
+
+      system_libraries = target.system_libraries.to_a
+
+      unless system_libraries.empty?
         EventHooks.run_hook :before_adding_system_library, target
 
         native_target = @context.native_object_for(target)
-
-        system_libraries = target.system_libraries
         native_target.add_system_libraries(system_libraries)
       end
     end
