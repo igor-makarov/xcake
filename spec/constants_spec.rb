847404a86b49f05210551a9188c45981d5e15c9b
Igor Makarov <igormaka@gmail.com>
2018-12-22 13:28:56 +0200

    fix test

--- spec/constants_spec.rb
+++ spec/constants_spec.rb
@@ -25,9 +25,9 @@ module Xcake
         settings1 = @helper.common_build_settings(:release, :ios, nil, nil)
         settings2 = @helper.common_build_settings(:release, :ios, nil, nil)
 
-        expect(settings_1.object_id).to_not eq(settings_2.object_id)
-        expect(settings_1['SDKROOT'].object_id).to_not eq(settings_2['SDKROOT'].object_id)
-        expect(settings_1['SDKROOT'][1].object_id).to_not eq(settings_2['SDKROOT'][1].object_id)
+        expect(settings1.object_id).to_not eq(settings2.object_id)
+        expect(settings1['SDKROOT'].object_id).to_not eq(settings2['SDKROOT'].object_id)
+        expect(settings1['SDKROOT'][1].object_id).to_not eq(settings2['SDKROOT'][1].object_id)
       end
     end
   end
