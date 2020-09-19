eb9b73b3747e7be807b682c1736cfebaf63f2d53
comex <comexk@gmail.com>
2020-09-19 09:42:59 -0400

    Add .cc to PBXSourcesBuildPhase (#219)
    Some C++ projects use .cc for source files instead of .cpp.

--- lib/xcake/path_classifier.rb
+++ lib/xcake/path_classifier.rb
@@ -7,7 +7,7 @@ module Xcake
     EXTENSION_MAPPINGS = {
       PBXFrameworksBuildPhase: %w(.a .dylib .so .framework).freeze,
       PBXHeadersBuildPhase: %w(.h .hpp).freeze,
-      PBXSourcesBuildPhase: %w(.c .m .mm .cpp .swift .xcdatamodeld .java).freeze,
+      PBXSourcesBuildPhase: %w(.c .m .mm .cpp .cc .swift .xcdatamodeld .java).freeze,
       PBXResourcesBuildPhase: %w(.xcassets).freeze
     }.freeze
 
